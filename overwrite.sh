624bcf42647f7e72dcbd7a0d1741fc87595d2672
Riroaki <2569126376@qq.com>
2018-12-22 00:37:25 +0800

    Improve file structure in overwrite.sh

--- overwrite.sh
+++ overwrite.sh
@@ -1,8 +1,8 @@
 cp ./codes/unistd_from_usr.h		/usr/include/asm-generic/unistd.h
-cp ./codes/unistd_from_linux-4.8.h	./linux-4.8/include/uapi/asm-generic/unistd.h
-cp ./codes/syscall_64.tbl		./linux-4.8/arch/x86/entry/syscalls/
-cp ./codes/mm.h				./linux-4.8/include/linux/
-cp ./codes/sched.h			./linux-4.8/include/linux/
-cp ./codes/fork.c			./linux-4.8/kernel/
-cp ./codes/fault.c			./linux-4.8/arch/x86/mm/
-cp ./codes/sys.c			./linux-4.8/kernel/sys.c
+cp ./codes/unistd_from_linux-4.8.h	/linux-4.8/include/uapi/asm-generic/unistd.h
+cp ./codes/syscall_64.tbl		/linux-4.8/arch/x86/entry/syscalls/
+cp ./codes/mm.h				/linux-4.8/include/linux/
+cp ./codes/sched.h			/linux-4.8/include/linux/
+cp ./codes/fork.c			/linux-4.8/kernel/
+cp ./codes/fault.c			/linux-4.8/arch/x86/mm/
+cp ./codes/sys.c			/linux-4.8/kernel/sys.c
